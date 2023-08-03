99c81e835b74bd38e08f4d1e513a39dd4ab969bd
Kévin JAMET <94893900+kevinjmt@users.noreply.github.com>
2023-08-03 08:55:12 +0200

    v0.1
    updated home
    finished toutes_les_recettes
    finished 1 receipt page

---
+++ toutes_les_recettes/seach.js
@@ -0,0 +1,21 @@
+const search = () => {
+    console.log("cacaprout")
+    const searchbox = document.getElementById("recherche-input").value.toUpperCase();
+    const storeitems = document.getElementById("receipt-list");
+    const product = document.querySelectorAll(".receipt-link");
+    const pname = storeitems.getElementsByTagName("h2");
+
+    for (var i = 0; i < pname.length; i++){
+        let match = product[i].getElementsByTagName('h2')[0];
+
+        if (match){
+            let textvalue = match.textContent || match.innerHTML;
+
+            if (textvalue.toUpperCase().indexOf(searchbox) > -1){
+                product[i].style.display = "";
+            } else{
+                product[i].style.display = "none";
+            }
+        }
+    }
+}
